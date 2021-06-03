f34c3429db96ab8e6ad4500721007b3f810b72ad
stnqls <stnqls3938@gmail.com>
2021-06-03 15:41:13 +0900

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,12 @@ module.exports = {
           'postcss-loader',
           'sass-loader'
         ]
+      },
+      {
+        test: /\.js$/,
+        use : [
+          'babel-loader'
+        ]
       }
     ]
   },
